fd6be7f6eea956feecd6d70b712ce54492f12eff
YevhenB0512 <evgenijboychuk@gmail.com>
2024-02-27 15:04:08 +0300

    creating and register models Task and User

--- todo_list/tasks/models.py
+++ todo_list/tasks/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Задание')
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
+    complete = models.BooleanField(default=False, verbose_name='Выполнено')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, blank=True, null=True,
+                             verbose_name='Пользователь')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'task'
+        ordering = ['complete']
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
--- todo_list/users/models.py
+++ todo_list/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    position = models.CharField(max_length=150, blank=True, null=True, verbose_name='Должность')
+
+    class Meta:
+        db_table = 'user'
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}' if self.first_name and self.first_name else self.username
